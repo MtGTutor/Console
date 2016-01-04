6edf1589d99a791588ed07f7ab60c83fea8afd4e
Pascal Kleindienst <mail@pascalkleindienst.de>
2016-01-04 10:35:22 +0100

    added server status test

---
+++ tests/Commands/ServerStatusTest.php
@@ -0,0 +1,76 @@
+<?php namespace MtGTutor\Console\Test\Commands;
+
+use MtGTutor\Console\Test\CommandTesterTrait;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ServerStatusTest
+ * @package MtGTutor\Console\Test\Commands
+ */
+class ServerStatusTest extends \PHPUnit_Framework_TestCase
+{
+    use CommandTesterTrait;
+
+    /**
+     * Sets command
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->setCommandTester('server:status');
+    }
+
+    /**
+     * Tests default execution
+     */
+    public function testExecute()
+    {
+        $this->tester->execute(['command' => $this->command->getName()]);
+
+        $this->assertRegExp('/\bonline\b/i', $this->tester->getDisplay());
+    }
+
+    /**
+     * Test Port option
+     */
+    public function testPortOption()
+    {
+        // long option
+        $this->tester->execute([
+            'command' => $this->command->getName(),
+            '--port'    => 80,
+        ]);
+
+        $this->assertRegExp('/\bonline\b/i', $this->tester->getDisplay());
+
+        // short syntax
+        $this->tester->execute([
+            'command' => $this->command->getName(),
+            '-p'    => 80,
+        ]);
+
+        $this->assertRegExp('/\bonline\b/i', $this->tester->getDisplay());
+    }
+
+    /**
+     * If server is offline there must be the offline notice
+     * Simulated via unused port
+     */
+    public function testIfServerIfOffline()
+    {
+        $this->tester->execute(
+            [
+                'command' => $this->command->getName(),
+                '-p'    => 1234, //unused port
+            ],
+            [
+                'verbosity' => OutputInterface::VERBOSITY_VERBOSE
+            ]
+        );
+
+        $this->assertRegExp('/\boffline\b/i', $this->tester->getDisplay());
+
+        // Verbose output -> Error number
+        $this->assertRegExp('/\(\d+\)/i', $this->tester->getDisplay());
+    }
+}
