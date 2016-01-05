cd68646bcbf0451433ec5b30c72c58fb5a53f872
Pascal Kleindienst <mail@pascalkleindienst.de>
2016-01-05 12:32:16 +0100

    refactored set info command

--- src/Commands/SetInfos.php
+++ src/Commands/SetInfos.php
@@ -26,12 +26,34 @@ class SetInfos extends Command
     protected $imagePath = 'http://magic.wizards.com/sites/mtg/files/';
 
     /**
+     * @var bool
+     */
+    protected $exists = false;
+
+    /**
+     * @var array
+     */
+    protected $data = [
+        'code'   => null,
+        'name'   => null,
+        'block'  => null,
+        'number' => null,
+        'date'   => null,
+        'logo'   => null,
+        'icon'   => null,
+    ];
+
+    /**
      * Configure command
      */
     protected function configure()
     {
         $this->setName('set:info')
-            ->addArgument('set', InputArgument::REQUIRED, 'Which set do you want to select. Use the official three-letter code.')
+            ->addArgument(
+                'set',
+                InputArgument::REQUIRED,
+                'Which set do you want to select. Use the official three-letter code.'
+            )
             ->addOption('full-path', null, InputOption::VALUE_NONE, 'Add the full path to the images')
             ->addOption('date-format', null, InputOption::VALUE_OPTIONAL, 'Date Format', 'Y-m-d')
             ->addOption('json', null, InputOption::VALUE_NONE, 'Output result as json')
@@ -76,112 +98,130 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // init vars
-        $exists = false;
-        $data = [
-            'code' => $input->getArgument('set'),
-            'name' => null,
-            'block' => null,
-            'number' => null,
-            'date' => null,
-            'logo' => null,
-            'icon' => null,
-        ];
+        $this->data['code'] = $input->getArgument('set');
 
         // Fetch WotC website
         $client = new Client();
         $crawler = $client->request('GET', $this->url);
 
-        $crawler->filter('.card-set-archive-table > ul > li > a > span.logo > img')->each(function (Crawler $node) use (&$client, &$data, &$exists) {
+        $crawler->filter('.card-set-archive-table > ul > li > a > span.logo > img')->each(function (Crawler $node) use (
+            &$client
+        ) {
             $logo = $node->attr('src');
+            $this->exists = true;
 
-            if(strpos(strtolower($logo), strtolower($data['code'])) !== false) {
-                // Set exists
-                $exists = true;
-
+            if ($this->setExists($logo)) {
                 // Get Logo + Icon + Name
                 $siblings = $node->parents()->first()->siblings();
 
-                $data['logo'] = str_replace($this->imagePath, '', $logo);
-                $data['icon'] = str_replace($this->imagePath, '', $siblings->eq(0)->children()->attr('src'));
-                $data['name'] = trim($siblings->eq(1)->text());
+                $this->data['logo'] = str_replace($this->imagePath, '', $logo);
+                $this->data['icon'] = str_replace($this->imagePath, '', $siblings->eq(0)->children()->attr('src'));
+                $this->data['name'] = trim($siblings->eq(1)->text());
 
-                // Go to details page
-                $link = $node->parents()->parents()->link();
-                $crawler = $client->click($link);
-
-                // Go to to info page (if exists)
-                $anchor = $crawler->selectLink('Info');
-                if ($anchor->count()) {
-                    $link = $anchor->link();
-                    $crawler = $client->click($link);
-                }
+                // go to next page
+                $crawler = $this->crawlNextPage($node, $client);
 
                 // Fetch block, number of cards and release date
-                $crawler->filter('.tab-content.current > p')->each(function (Crawler $node, $i) use (&$data) {
+                $crawler->filter('.tab-content.current > p')->each(function (Crawler $node, $i) {
                     if ($i % 2) {
-                        // Get block and number of cards
-                        if ($i == 1) {
-                            preg_match('~<strong>Block:<\\/strong>.*<em>(.+?)</~m', $node->html(), $block);
-                            preg_match('~<strong>Number of Cards:\s*<\/strong>[^\d]*(\d+)<?~m', $node->html(), $number);
-
-                            $data['block'] = $block[1];
-                            $data['number'] = (int)$number[1];
-                        }
-
-                        // get release date
-                        if ($i == 5) {
-                            preg_match('~<strong>Release Date:<\/strong>.+?([\w|,|\s]+)<~im', $node->html(), $release);
-                            $data['date'] = $release[1];
-                        }
+                        $this->fetchBlockCardsAndDate($node, $i);
                     }
                 });
             }
         });
 
         // Could not find set
-        if (!$exists) {
-            throw new \InvalidArgumentException('Could not find set with the following code: ' . $data['code']);
+        if (!$this->exists) {
+            throw new \InvalidArgumentException('Could not find set with the following code: ' . $this->data['code']);
         }
 
         // Format result
-        $data = $this->formatResult($data, $input);
+        $this->formatResult($input);
 
         // Ouput result
-        $this->output($data, $input, $output);
+        $this->output($input, $output);
+    }
+
+    /**
+     * @param string $url
+     * @return bool|null
+     */
+    protected function setExists($url)
+    {
+        return strpos(strtolower($url), strtolower($this->data['code'])) !== false;
+    }
+
+    /**
+     * @param \Symfony\Component\DomCrawler\Crawler $crawler
+     * @param \Goutte\Client                        $client
+     * @return \Symfony\Component\DomCrawler\Crawler
+     */
+    protected function crawlNextPage(Crawler $crawler, Client $client)
+    {
+        // Go to details page
+        $link = $crawler->parents()->parents()->link();
+        $crawler = $client->click($link);
+
+        // Go to to info page (if exists)
+        $anchor = $crawler->selectLink('Info');
+        if ($anchor->count()) {
+            $link = $anchor->link();
+            $crawler = $client->click($link);
+        }
+
+        return $crawler;
+    }
+
+    /**
+     * @param \Symfony\Component\DomCrawler\Crawler $node
+     * @param integer                               $i
+     */
+    protected function fetchBlockCardsAndDate(Crawler $node, $i)
+    {
+        // Get block and number of cards
+        if ($i == 1) {
+            preg_match('~<strong>Block:<\\/strong>.*<em>(.+?)</~m', $node->html(), $block);
+            preg_match('~<strong>Number of Cards:\s*<\/strong>[^\d]*(\d+)<?~m', $node->html(), $number);
+
+            $this->data['block'] = $block[1];
+            $this->data['number'] = (int)$number[1];
+        }
+
+        // get release date
+        if ($i == 5) {
+            preg_match('~<strong>Release Date:<\/strong>.+?([\w|,|\s]+)<~im', $node->html(), $release);
+            $this->data['date'] = $release[1];
+        }
     }
 
     /**
      * Format result
-     * @param array                                           $data
      * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @return array
+     * @return void
      */
-    protected function formatResult(array $data, InputInterface $input)
+    protected function formatResult(InputInterface $input)
     {
         // change date format
-        $date = new \DateTime(date('Y-m-d H:i:s', strtotime($data['date'])));
-        $data['date'] = $date->format($input->getOption('date-format'));
+        $date = new \DateTime(date('Y-m-d H:i:s', strtotime($this->data['date'])));
+        $this->data['date'] = $date->format($input->getOption('date-format'));
 
         // add full path to url
         if ($input->getOption('full-path')) {
-            $data['logo'] = $this->imagePath . $data['logo'];
-            $data['icon'] = $this->imagePath . $data['icon'];
+            $this->data['logo'] = $this->imagePath . $this->data['logo'];
+            $this->data['icon'] = $this->imagePath . $this->data['icon'];
         }
-
-        return $data;
     }
 
     /**
      * Write result to console
-     * @param array                                             $data
      * @param \Symfony\Component\Console\Input\InputInterface   $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      */
-    protected function output(array $data, InputInterface $input, OutputInterface $output)
+    protected function output(InputInterface $input, OutputInterface $output)
     {
         // json output
         if ($input->getOption('json')) {
-            $output->writeln(json_encode($data, JSON_PRETTY_PRINT));
+            $output->writeln(json_encode($this->data, JSON_PRETTY_PRINT));
         }
 
         // table output
@@ -189,7 +229,7 @@ EOT
             $table = new Table($output);
             $table
                 ->setHeaders(['Code', 'Name', 'Block', 'Number', 'Release Date', 'Logo', 'Icon'])
-                ->setRows([$data]);
+                ->setRows([$this->data]);
             $table->render();
         }
     }
